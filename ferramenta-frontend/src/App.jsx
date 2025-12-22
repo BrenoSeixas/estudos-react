48ed72da6a79b1b906e1fb182f3fdcaab7aa4bd6
Breno Seixas <olegarioseixasb@gmail.com>
2025-12-22 18:58:43 -0300

    Componentes

--- ferramenta-frontend/src/App.jsx
+++ ferramenta-frontend/src/App.jsx
@@ -1,64 +1,28 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Form from "./Form/form";
 
-// const App = () => {
-//   const livros = [
-//     { nome: "A Game of Thrones", ano: 1996 },
-//     { nome: "A Clash of Kings", ano: 1998 },
-//     { nome: "A Storm of Swords", ano: 2000 },
-//   ];
-//   return (
-//     <ul>
-//       {livros
-//         .filter(({ ano }) => ano >= 1998)
-//         .map(({ nome, ano }) => (
-//           <li key={nome}>
-//             {nome}, {ano}
-//           </li>
-//         ))}
-//     </ul>
-//   );
-// };
-
-// Organize os produtos como mostrado no vídeo
-// Mostre apenas produtos que forem mais caros que R$ 1500
-const produtos = [
-  {
-    id: 1,
-    nome: "Smartphone",
-    preco: "R$ 2000",
-    cores: ["#29d8d5", "#252a34", "#fc3766"],
-  },
-  {
-    id: 2,
-    nome: "Notebook",
-    preco: "R$ 3000",
-    cores: ["#ffd045", "#d4394b", "#f37c59"],
-  },
-  {
-    id: 3,
-    nome: "Tablet",
-    preco: "R$ 1500",
-    cores: ["#365069", "#47c1c8", "#f95786"],
-  },
-];
+const Teste = () => {
+  const active = false;
+  if (active) {
+    return <p>Teste</p>;
+  } else {
+    return null;
+  }
+};
 
 const App = () => {
-  const dados = produtos.filter(({ preco }) =>
-    Number(preco.replace("R$ ", "") > 1500)
-  );
   return (
-    <section>
-    {dados.map(({id,nome,preco,cores}) => (
-      <div key={id}>
-      <h1>{nome}</h1>
-      <p>Preço: {preco}</p>
-      <ul>{cores.map((cor) =>(
-        <li style={{background: cor, color: 'white'}} key={cor}>{cor}</li>
-      ))}</ul>
-      </div>
-    ))}
-    </section>
+    <React.Fragment>
+      <Teste />
+      <Header />
+      <Form />
+      <Footer />
+    </React.Fragment>
   );
+
+  // return <button onClick={(event) => console.log(event.target)}>Clique</button>;
 };
 
 export default App;
