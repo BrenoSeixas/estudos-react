1e6ec085809a9b8eda269114eb420977af38ffe0
Breno Seixas <olegarioseixasb@gmail.com>
2025-12-22 17:39:41 -0300

    JXS Arrays

--- ferramenta-frontend/src/App.jsx
+++ ferramenta-frontend/src/App.jsx
@@ -1,87 +1,64 @@
 import React from "react";
 
-//const titulo = <h1>Esse é um título</h1>;
+// const App = () => {
+//   const livros = [
+//     { nome: "A Game of Thrones", ano: 1996 },
+//     { nome: "A Clash of Kings", ano: 1998 },
+//     { nome: "A Storm of Swords", ano: 2000 },
+//   ];
+//   return (
+//     <ul>
+//       {livros
+//         .filter(({ ano }) => ano >= 1998)
+//         .map(({ nome, ano }) => (
+//           <li key={nome}>
+//             {nome}, {ano}
+//           </li>
+//         ))}
+//     </ul>
+//   );
+// };
 
-// Mostre os dados da aplicação, como aprensetado no vídeo
-// Não utilize CSS externo, use o style para mudar as cores
-// Se a situação estiver ativa pinte de verde, inativa vermelho
-// Se o gasto for maior que 10000 mostre uma mensagem
-const luana = {
-  cliente: "Luana",
-  idade: 27,
-  compras: [
-    { nome: "Notebook", preco: "R$ 2500" },
-    { nome: "Geladeira", preco: "R$ 3000" },
-    { nome: "Smartphone", preco: "R$ 1500" },
-  ],
-  ativa: true,
-};
-
-const mario = {
-  cliente: "Mario",
-  idade: 31,
-  compras: [
-    { nome: "Notebook", preco: "R$ 2500" },
-    { nome: "Geladeira", preco: "R$ 3000" },
-    { nome: "Smartphone", preco: "R$ 1500" },
-    { nome: "Guitarra", preco: "R$ 3500" },
-  ],
-  ativa: false,
-};
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+  {
+    id: 1,
+    nome: "Smartphone",
+    preco: "R$ 2000",
+    cores: ["#29d8d5", "#252a34", "#fc3766"],
+  },
+  {
+    id: 2,
+    nome: "Notebook",
+    preco: "R$ 3000",
+    cores: ["#ffd045", "#d4394b", "#f37c59"],
+  },
+  {
+    id: 3,
+    nome: "Tablet",
+    preco: "R$ 1500",
+    cores: ["#365069", "#47c1c8", "#f95786"],
+  },
+];
 
 const App = () => {
-  const dados = mario;
-
-  const total = dados.compras
-    .map((item) => +item.preco.replace("R$ ", ""))
-    .reduce((a, b) => a + b);
-
+  const dados = produtos.filter(({ preco }) =>
+    Number(preco.replace("R$ ", "") > 1500)
+  );
   return (
-    <div>
-      <p>Nome: {dados.cliente}</p>
-      <p>Nome: {dados.idade}</p>
-      <p>
-        Situação:{" "}
-        <span style={{ color: dados.ativa ? "green" : "red" }}>
-          {" "}
-          {dados.ativa ? "Ativa" : "Inativa"}
-        </span>
-      </p>
-      <p>Total: R$ {total}</p>
-      {total > 10000 && <p>Você está gastando muito!</p>}
-    </div>
+    <section>
+    {dados.map(({id,nome,preco,cores}) => (
+      <div key={id}>
+      <h1>{nome}</h1>
+      <p>Preço: {preco}</p>
+      <ul>{cores.map((cor) =>(
+        <li style={{background: cor, color: 'white'}} key={cor}>{cor}</li>
+      ))}</ul>
+      </div>
+    ))}
+    </section>
   );
 };
 
 export default App;
-
-//   const random = Math.random();
-//   const ativo = true;
-
-//   const teste = (sobrenome) => {
-//     return "Breno" + sobrenome;
-//   };
-
-//   const Carro = {
-//     marca: "honda",
-//     rodas: "4",
-//   };
-
-// const estiloP={
-//   color: 'blue',
-//   fontSize: '2rem',
-// }
-
-//   return (
-//     <>
-//       {titulo}
-//       <p>
-//         {true ? "sim" : "não"} - {10}
-//       </p>
-//       {teste(" Seixas")}
-//       <p>{Carro.marca}</p>
-//       <p>{Carro.rodas}</p>
-//       <p style={estiloP}>{new Date().getFullYear()}</p>
-//       <p className={ativo ? "ativo" : "inativo"}>{(random * 1000) / 50}</p>
-//     </>
-//   );
