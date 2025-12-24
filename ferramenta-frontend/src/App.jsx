531095c11612f2c595a1ad810654a847ab39d83e
Breno Seixas <olegarioseixasb@gmail.com>
2025-12-24 00:05:59 -0300

    Desafio Componentes

--- ferramenta-frontend/src/App.jsx
+++ ferramenta-frontend/src/App.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Header from "./Header";
-import Footer from "./Footer";
-import Form from "./Form/form";
+import Home from "./Home";
+import Produtos from "./Produtos";
+
+// Replique a interface como a apresentada na aula
+// Utilize a array abaixo para mostrar os produtos
+// Quebre em componentes o que precisar ser reutilizado
+// Dica: const { pathname } = window.location; (puxa o caminho do URL)
 
 const App = () => {
+  const { pathname } = window.location;
+  let Pagina = Home;
+
+  if (pathname === "/produtos") {
+    Pagina = Produtos;
+  } else {
+    Pagina = Home;
+  }
+
   return (
     <div>
-      <Form />
+      <Header />
+      <Pagina />
     </div>
   );
 };
